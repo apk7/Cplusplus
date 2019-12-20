01fa3d3985fc70a0e4b74ccfc16cc5f21833e917
apurv <apurvkulkarni7@gmail.com>
2019-12-20 15:37:28 +0100

    Completed polynomial class and its function

---
+++ Polynomials/main.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include "polynomial.cc"
+
+using namespace std;
+int main(){
+
+    Polynomial a({1,2,3});
+    Polynomial b({1,2,3,4,5});
+    Polynomial c({1,1,1});
+
+    // a.degree();
+    // cout<<"a2 = "<<a[2]<<endl;
+    // cout<<power(2,10)<<endl;
+    // cout<<"addition = "<<(a[2] + b[2])<<endl;
+    // vector<value_type> test(1,0);
+    // vector<value_type> temp1(1,2);
+    // cout<<temp1[5]<<endl;
+    // test[0] = 2 + 3;
+    // Polynomial add = a + b;
+    // cout<<add.degree()<<endl;
+    // cout<<add[3]<<endl;
+    // Polynomial sub = a - b;
+    // cout<<sub.degree()<<endl;
+    // cout<<sub[4]<<endl;
+    // a[3] = 0;
+    // cout<<power(2,2);
+
+
+    cout<<a;
+
+}
